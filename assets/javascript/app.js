8001a9dd2e4528c1cc896567a43d7a86514d1deb
blupie01 <jeffrey.chou.89@gmail.com>
2017-01-20 12:36:25 -0500

    first push

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB8hJRJeScaWopXWCzPEgG7UA0VnG5Vo0E",
+    authDomain: "trainspotter-45428.firebaseapp.com",
+    databaseURL: "https://trainspotter-45428.firebaseio.com",
+    storageBucket: "trainspotter-45428.appspot.com",
+    messagingSenderId: "642093108830"
+};
+firebase.initializeApp(config);
+
+//var to hold database info
+var trainDataBase = firebase.database();
